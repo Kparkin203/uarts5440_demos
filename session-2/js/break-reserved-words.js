8f6d27657e2e0a8906419739fd55a39945420613
TJ Nicolaides <tjnicolaides@gmail.com>
2013-05-23 22:57:55 -0400

    Demo for reserved words

---
+++ session-2/js/break-reserved-words.js
@@ -0,0 +1,39 @@
+//var switch = 4; // this isn't okay
+
+var myObject = {
+	"switch" : 4 // this is okay
+}
+
+
+/*
+
+Complete list: https://developer.mozilla.org/en-US/docs/JavaScript/Reference/Reserved_Words
+
+break
+case
+catch
+continue
+debugger
+default
+delete
+do
+else
+finally
+for
+function
+if
+in
+instanceof
+new
+return
+switch
+this
+throw
+try
+typeof
+var
+void
+while
+with
+*/
+
