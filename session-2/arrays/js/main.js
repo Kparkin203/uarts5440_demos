311466270d0aec930841c868c2a92ec463925b65
TJ Nicolaides <tjnicolaides@gmail.com>
2013-05-24 01:09:21 -0400

    Arrays. And we loop through them, too.

---
+++ session-2/arrays/js/main.js
@@ -0,0 +1,8 @@
+var animals = ['Cow', 'Horse', 'Chicken', 'Pig', 'Dog'];
+console.log(animals);
+
+// let's say good night to all of our animals
+
+for(i = 0; i < animals.length; i++) {
+	console.log('Good night, ' + animals[i] + '.');
+}
