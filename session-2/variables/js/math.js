f3a998f103bd08577c03df8e9f9449244aa4c742
TJ Nicolaides <tjnicolaides@gmail.com>
2013-05-24 00:03:26 -0400

    All about VARIABLES

---
+++ session-2/variables/js/math.js
@@ -0,0 +1,20 @@
+// maths
+var num = 100;
+
+	num = num * 2;
+	console.log(num);
+	
+	num = num/4;
+	console.log(num);
+	
+	num = num + 3 - 20;
+	console.log(num);
+	
+// parentheses
+	
+var answer1 = 2 + 3 * 5;
+//console.log(answer1);
+
+var answer2 = (2 + 3) * 5;
+//console.log(answer2);
+
---
+++ session-2/variables/js/other-variables.js
@@ -0,0 +1,12 @@
+var num1 = 100;
+var num2 = num1;
+
+console.log(num2);
+
+num1 = num1 * 10;
+
+console.log(num2);
+
+num2 = num1;
+
+console.log(num2);
