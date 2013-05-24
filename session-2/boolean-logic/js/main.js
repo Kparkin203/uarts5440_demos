37deea647a3b274c7ebc14fdb1cae82fab233cf2
TJ Nicolaides <tjnicolaides@gmail.com>
2013-05-24 00:31:14 -0400

    Boolean logic

---
+++ session-2/boolean-logic/js/main.js
@@ -0,0 +1,24 @@
+// "falsy" or "false-ish" values = false, null, undefined, '', 0, NaN
+// "truthy" values = true, anything not falsy
+
+// here's a microwave. Just go with it:
+
+var cook_food = true;
+
+console.log(cook_food);
+
+if(cook_food) {
+	
+	console.log('your hot pocket is being cooked RIGHT NOW.');
+		
+} else {
+	
+	console.log('your hot pocket is like a frozen cheese brick');
+
+}
+
+// inequality ... >= <= > <
+// loose equality == !=
+// strict equality === !==
+// AND &&
+// OR ||
