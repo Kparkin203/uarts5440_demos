e0104d31e925526348469891ef8a633e09b2490e
TJ Nicolaides <tjnicolaides@gmail.com>
2013-06-13 13:34:20 -0400

    one more Session 2 change

--- session-2/06-arrays/js/main.js
+++ session-2/06-arrays/js/main.js
@@ -1,12 +1,26 @@
 var animals = ['Cow', 'Horse', 'Chicken', 'Pig', 'Dog'];
-console.log(animals);
+
+//console.log(animals);
 
 // let's say good night to all of our animals
 
-for(i = 0; i < animals.length; i++) {
-	console.log('Good night, ' + animals[i] + '.');
+document.write('<ol>');
+
+for(var i = 0; i < animals.length; i++) {
+	document.write('<li>Good night, ' + animals[i] + '.</li>');
 }
 
-var numbers = [1, 2, 3, 4, 5];
+document.write('</ol>');
 
-// square the numbers
+
+/*var numbers = [1, 2, 3, 4, 5];
+
+for (var i = 0; i < numbers.length; i++) {
+
+	//var squared = numbers[i] * numbers[i];
+	
+	//console.log(squared);
+	
+}
+
+// square the numbers*/
